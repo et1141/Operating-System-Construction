68dbc8112de95b39b76d5d38f2aed5a6933475d7
Dawid Grabowski <mdexor@gmail.com>
2024-04-24 12:33:54 +0200

    Only one CGA_Screen

--- main.cc
+++ main.cc
@@ -14,25 +14,24 @@ int main()
 {	
 	/**/		
 	CGA_Stream cga;
-	CGA_Screen screen; 
 	
 	int x,y;
 
 	//1. CGA_Screen test:
-	screen.setpos(0,0);
+	cga.setpos(0,0);
 	for(int i=0;i<80*24;i++){
-		screen.print("1",1,4);
+		cga.print("1",1,4);
 	}
 
-	screen.print("\n",1,4);
+	cga.print("\n",1,4);
 	for(int i=0;i<77;i++){
-		screen.print("4",1,7);
+		cga.print("4",1,7);
 	}
-	screen.print("bsfff",5,8);
-	screen.print("\n",1,4);
-	screen.print("bsfff",5,8);
-	screen.setpos(0,23);
-	screen.print("\n",1,11);
+	cga.print("bsfff",5,8);
+	cga.print("\n",1,4);
+	cga.print("bsfff",5,8);
+	cga.setpos(0,23);
+	cga.print("\n",1,11);
 
 		
 	//cga.flush();
@@ -67,7 +66,7 @@ int main()
 
 
     // Print instructions
-    screen.print("\nYou are in while true loop for keyboard testing. Click q to quit", 66, 4);
+    cga.print("\nYou are in while true loop for keyboard testing. Click q to quit", 66, 4);
 
     // Main loop to wait for key presses
     while (true) {
@@ -82,7 +81,7 @@ int main()
  	    char pressedKey[2]; 
         pressedKey[0] = (char)key.ascii(); 
         pressedKey[1] = '\0'; //1st arg of screen.print needs to be char* (not char) 
-        screen.print(pressedKey, 1, 6); 
+        cga.print(pressedKey, 1, 6); 
         }
     }
 
