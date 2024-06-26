e4d6d6bb59f0cac14c8e81586a9578ddcbea693e
Dawid <mdexor@gmail.com>
2024-06-26 15:21:56 +0200

    Create Thread class

--- syscall/thread.h
+++ syscall/thread.h
@@ -12,15 +12,12 @@
 #define __thread_include__
 
 /* Add your code here */ 
- 
-class Thread
-/* Add your code here */ 
- 
-{
+ #include "entrant.h"
+class Thread : public Entrant {
 public:
+    // Constructor that initializes the Thread with a top-of-stack pointer
 	Thread(const Thread &copy) = delete; // prevent copying
-/* Add your code here */ 
- 
+    Thread(void* tos) : Entrant(tos) {}
 };
 
 #endif
