ff0a4026609966e84465f26c28f5eb890636535c
Dawid <mdexor@gmail.com>
2024-07-02 19:36:30 +0200

    Create Watch class

--- device/watch.cc
+++ device/watch.cc
@@ -9,9 +9,32 @@
 /* process switch if necessary.                                              */
 /*****************************************************************************/
 
-/* Add your code here */ 
-/* Add your code here */ 
- 
-/* Add your code here */ 
-/* Add your code here */ 
+#include "watch.h"
+#include "plugbox.h"
+#include "pic.h"
+#include "guarded_scheduler.h"
+#include "user/globals.h"
 
+Watch::Watch(int us) : PIT(us) {
+    // Initialize the PIT with the given interval
+}
+
+void Watch::windup() {
+    // Register the Watch object with the Plugbox
+    plugbox.assign(plugbox.timer, *this);
+
+    // Allow timer interrupts using the PIC
+    pic.allow(pic.timer);
+}
+
+bool Watch::prologue() {
+    // Prologue of the interrupt handler
+    // Handle the beginning of the interrupt
+    // For simplicity, always return true to indicate that the epilogue should be called
+    return true;
+}
+
+void Watch::epilogue() {
+    // Trigger the process switch
+    scheduler.resume();
+}
