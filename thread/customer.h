698eb1ccc37d9dc602c2c5bdfd7ed53413578899
Dawid <mdexor@gmail.com>
2024-07-09 13:36:35 +0200

    Create customer

--- thread/customer.h
+++ thread/customer.h
@@ -12,15 +12,51 @@
 #ifndef __customer_include__
 #define __customer_include__
 
-/* Add your code here */ 
 
-class Customer
-/* Add your code here */ 
-{
-public:
-	Customer (const Customer &copy) = delete; // prevent copying
+#include "thread/entrant.h"
+#include "meeting/waitingroom.h"
+#include "device/cgastr.h"
+
+extern CGA_Stream kout;
+
+/**
+ * @brief The Customer class represents a process that can wait for events in a waiting room.
+ */
+class Customer : public Entrant {
 private:
-/* Add your code here */ 
+    Customer(const Customer &copy); // Prevent copying
+
+    Waitingroom* waitingroom;
+
+public:
+    /**
+     * @brief Constructor that forwards the tos parameter to the base class Entrant constructor.
+     * @param tos Top of stack pointer.
+     */
+    Customer(void* tos) : Entrant(tos), waitingroom(0) {}
+
+    /**
+     * @brief Marks that the customer is currently waiting in the specified waiting room.
+     * @param w Pointer to the Waitingroom object.
+     */
+    void waiting_in(Waitingroom* w) {
+    if ((w != 0) && (waitingroom != 0)) {
+        kout << endl << "waitingroom is not NULL!" << endl;
+        kout.flush();
+        return;
+    }
+    waitingroom = w;
+}
+
+    /**
+     * @brief Returns the Waitingroom object in which the customer is currently registered.
+     * @return Pointer to the Waitingroom object. Returns 0 if the customer is not waiting for any event.
+     */
+    Waitingroom* waiting_in() {
+    return waitingroom;
+}
 };
 
-#endif
+#endif // __customer_include__
+
+
