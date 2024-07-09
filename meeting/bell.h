5099fedda8ff20c66072f6ae7aea3c99a5fca014
Dawid <mdexor@gmail.com>
2024-07-09 13:09:30 +0200

    Create bell

--- meeting/bell.h
+++ meeting/bell.h
@@ -13,15 +13,43 @@
 #ifndef __Bell_include__
 #define __Bell_include__
 
-/* Add your code here */ 
+#include "object/chain.h"
+#include "device/cgastr.h"
 
+extern CGA_Stream cout; 
 class Bell
-/* Add your code here */ 
+: public Chain
 {
+private:
+	Bell(const Bell &copy); // prevent copying
+	int counter; 
+
 public:
-	Bell(const Bell &copy) = delete; // prevent copying
-	Bell() {}
-/* Add your code here */ 
+	Bell():counter(0) {}
+
+	// Set or get the counter
+	void wait (int value) {
+		counter = value;
+	}
+
+	int wait () {
+		return counter;
+	}
+
+	void tick () {
+		--counter;
+	}
+
+	// Check whether the counter has run down.
+	bool run_down() {
+		if (counter <= 0)
+			return true;
+		return false;
+
+	}
+
+	// The bell rings
+	virtual void ring () = 0;
 };
 
 #endif
